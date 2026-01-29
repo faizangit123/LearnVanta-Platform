d29b50b030291ecf534144322162dd377474bb66
MD Faizan <faizanrock705@gmail.com>
2026-01-29 14:04:59 +0530

    test1

--- Django-backend/edustream_backend/settings.py
+++ Django-backend/edustream_backend/settings.py
@@ -236,3 +236,18 @@ try:
     from .settings_local import *
 except ImportError:
     pass
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
